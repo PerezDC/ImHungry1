f900e74911693756741cafe20bfc2aa8ff60c090
PerezDC <61435556+PerezDC@users.noreply.github.com>
2020-09-21 00:11:52 -0700

    Added AppDbContext

---
+++ ImHungry/Models/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImHungry.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
